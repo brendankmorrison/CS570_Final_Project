a6208cfc97c807d795ff3cc67d1eb410fc1ce6ab
Brendan <47620345+brendankmorrison@users.noreply.github.com>
2022-05-06 15:03:36 -0700

    add string generation

--- final_project.py
+++ final_project.py
@@ -1,5 +1,5 @@
 
-fileName = "datapoints/in2.txt"
+fileName = "datapoints/in1.txt"
 x = ""
 y = ""
 arrNum = 1
@@ -21,14 +21,25 @@ with open(fileName) as file:
             else: 
                 arr2.append(int(line))
 
-# Generate strings
 
+print("base x: " + x)
+print(arr1)
+print("base y: " + y)
+print(arr2)
+# Generate string x
+for index in arr1:
+    x = x[:index + 1] + x + x[index + 1:]
 
+x = x.replace(' ', '').replace('\n', '')
+
+for index in arr2:
+    y = y[:index + 1] + y + y[index + 1:]
+
+y = y.replace(' ', '').replace('\n', '')
 
 print(x)
 print(y)
-print(arr1)
-print(arr2)
+
 
 
 
